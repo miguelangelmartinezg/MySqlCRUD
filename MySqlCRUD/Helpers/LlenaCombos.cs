5614f04d26aaf7850d46c75b6065ef0efb770da4
Miguel Angel Martinez Gonzalez <miguelangelmartinezg@hotmail.com>
2021-01-20 15:26:31 +0100

    Ultimo desde Insude 200012121

--- MySqlCRUD/Helpers/LlenaCombos.cs
+++ MySqlCRUD/Helpers/LlenaCombos.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MySqlCRUD.Models;
+using MySqlCRUD.Models.ViewModels;
 
 namespace MySqlCRUD.Helpers
 {
@@ -15,18 +16,44 @@ namespace MySqlCRUD.Helpers
             List<SelectListItem> oList = null;
             using (crudEntities1 DB = new crudEntities1())
             {
-                oList = DB.mitabla.Select(d => new SelectListItem
-                {
-                    Text = d.nombre,
-                    Value = d.id.ToString()
-                }).OrderBy(d => d.Text)
-                    .ToList();
+                
+                //oList = DB.mitabla.Select(d => new SelectListItem
+                //{
+                //    Text = d.nombre,
+                //    Value = d.id.ToString()
+                //}).OrderBy(d => d.Text)
+                //    .ToList();
 
-                oList.Insert(0, new SelectListItem
+                //oList.Insert(0, new SelectListItem
+                //{
+                //    Text = "Seleccione un Registro",
+                //    Value = "0"
+                //});
+                List <PadresViewModel> oPadres = null;
+                oPadres = (from d in DB.mitabla
+                         select new PadresViewModel
+                         {
+                             Id = d.id,
+                             Nombre = d.nombre
+                         }).ToList();
+                oList = oPadres.ConvertAll(d =>
                 {
-                    Text = "Seleccione un Registro",
-                    Value = "0"
+                    new SelectListItem(
+                    {
+                        Text = d.Nombre.ToString(),
+                        Value = d.Id.ToString(),
+
+                        oList.Insert(0, new SelectListItem
+                        {
+                            Text = "Seleccione un Registro",
+                            Value = "0"
+                        })
+                     })
+                        
+
+               
                 });
+                
             }
 
             return oList;
